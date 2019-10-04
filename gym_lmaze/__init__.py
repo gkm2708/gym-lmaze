ae78a9fe63e74416e41e9154f8c7c56b2ab4819f
Gaurav Kumar <gkumar@march.Compute.TechFak.DE>
2019-10-04 11:35:13 +0200

    added foveal environment

--- gym_lmaze/__init__.py
+++ gym_lmaze/__init__.py
@@ -4,3 +4,8 @@ register(
     id='lmaze-v0',
     entry_point='gym_lmaze.envs:LmazeEnv',
 )
+
+register(
+    id='lmaze-v1',
+    entry_point='gym_lmaze.envs:LmazeEnv_v1',
+)
--- gym_lmaze/envs/lmaze_env_v1.py
+++ gym_lmaze/envs/lmaze_env_v1.py
@@ -7,21 +7,26 @@ import matplotlib.pyplot as plt
 plt.ion()
 plt.show(block=False)
 
-class LmazeEnv(gym.Env):
+class LmazeEnv_v1(gym.Env):
   metadata = {'render.modes': ['human']}
 
   def __init__(self):
     print("init-init")
     self.action_space = spaces.Discrete(4)
-    self.realgrid = 12
+    self.realgrid = 14
     self.expansionRatio = 7
-    self.gridsize = self.realgrid * self.expansionRatio
+    self.fovea = 5
+    #self.gridsize = self.realgrid * self.expansionRatio
+    self.gridsize = self.fovea * self.expansionRatio
     self.observation_space = gym.spaces.Box(0.0, 1.0, shape=(4,self.gridsize,self.gridsize))
     self.negativeNominal = -1.0
     self.positiveNominal = 0.0
     self.positiveFull = 1.0
     self.RANDOM_BALL = False
     self.VISUALIZE = True
+    self.f_goal_x = 0
+    self.f_goal_y = 0
+    self.localDone = False
 
     """self.gri = np.array(  [['W', 'W', 'W', 'W', 'W', 'W', 'W', 'W'],
                             ['W', 'S', 'B', 'B', 'B', 'W', 'W', 'B'],
@@ -32,18 +37,20 @@ class LmazeEnv(gym.Env):
                             ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'W'],
                             ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'W']])"""
 
-    self.grid = np.array(  [['W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W'],
-                                ['W', 'S', 'B', 'B', 'B', 'W', 'B', 'B', 'B', 'B', 'W', 'W'],
-                                ['W', 'B', 'W', 'W', 'W', 'W', 'B', 'W', 'W', 'B', 'W', 'W'],
-                                ['W', 'B', 'W', 'B', 'W', 'B', 'B', 'B', 'W', 'B', 'W', 'W'],
-                                ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W', 'W'],
-                                ['W', 'B', 'B', 'B', 'W', 'X', 'W', 'B', 'W', 'W', 'W', 'W'],
-                                ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W', 'W'],
-                                ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W', 'W'],
-                                ['W', 'B', 'W', 'B', 'B', 'B', 'W', 'B', 'W', 'B', 'W', 'W'],
-                                ['W', 'B', 'W', 'W', 'W', 'W', 'W', 'B', 'W', 'B', 'W', 'W'],
-                                ['W', 'B', 'B', 'B', 'B', 'W', 'B', 'B', 'B', 'B', 'W', 'W'],
-                                ['W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W']])
+    self.grid = np.array(       [['W','W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W'],
+                                ['W','W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W'],
+                                ['W','W', 'S', 'B', 'B', 'B', 'W', 'B', 'B', 'B', 'B', 'W', 'W', 'W'],
+                                ['W','W', 'B', 'W', 'W', 'W', 'W', 'B', 'W', 'W', 'B', 'W', 'W', 'W'],
+                                ['W','W', 'B', 'W', 'B', 'W', 'B', 'B', 'B', 'W', 'B', 'W', 'W', 'W'],
+                                ['W','W', 'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W', 'W', 'W'],
+                                ['W','W', 'B', 'B', 'B', 'W', 'X', 'W', 'B', 'W', 'W', 'W', 'W', 'W'],
+                                ['W','W', 'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W', 'W', 'W'],
+                                ['W','W', 'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W', 'W', 'W'],
+                                ['W','W', 'B', 'W', 'B', 'B', 'B', 'W', 'B', 'W', 'B', 'W', 'W', 'W'],
+                                ['W','W', 'B', 'W', 'W', 'W', 'W', 'W', 'B', 'W', 'B', 'W', 'W', 'W'],
+                                ['W','W', 'B', 'B', 'B', 'B', 'W', 'B', 'B', 'B', 'B', 'W', 'W', 'W'],
+                                ['W','W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W'],
+                                ['W','W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W']])
 
     """
     self.grid = np.chararray((self.gridsize,self.gridsize))
@@ -70,15 +77,26 @@ class LmazeEnv(gym.Env):
     self.reset()
     print("init-end")
 
+
+
+
+
+
+
   def render(self, mode='human', close=False):
     if(mode == 'human'):
         self.VISUALIZE = True
+
+
+
+
+
+
 
   """             Reset-Initialize Handle           """
   """    
   """
   def reset(self):                        # just reset
-    #print("RESET")
 
     self.state = np.zeros((self.realgrid * self.realgrid * 4), dtype=np.float32)
     self.state[0 * self.realgrid * self.realgrid : 1 * self.realgrid * self.realgrid] = np.reshape(
@@ -94,7 +112,7 @@ class LmazeEnv(gym.Env):
             (-1))
 
     self.state[3 * self.realgrid * self.realgrid : 4 * self.realgrid * self.realgrid] = np.reshape(
-            np.asarray([[1.0 if cell == "B" else 0.0 for cell in row] for row in self.grid]),
+            np.asarray([[1.0 if cell == "B" or cell == "S" or cell == "X" else 0.0 for cell in row] for row in self.grid]),
             (-1))
 
     start = np.where(self.grid == 'S')
@@ -109,38 +127,108 @@ class LmazeEnv(gym.Env):
     self.stepCount = 0
     self.img = None
 
-    retState = np.concatenate((np.reshape(self.state[0 * self.realgrid * self.realgrid : 1 * self.realgrid * self.realgrid],
-                     ((1,self.realgrid,self.realgrid))),
-                          np.reshape(self.state[1 * self.realgrid * self.realgrid : 2 * self.realgrid * self.realgrid],
-                     ((1,self.realgrid,self.realgrid))),
-                          np.reshape(self.state[2 * self.realgrid * self.realgrid : 3 * self.realgrid * self.realgrid],
-                     ((1,self.realgrid,self.realgrid))),
-                          np.reshape(self.state[3 * self.realgrid * self.realgrid : 4 * self.realgrid * self.realgrid],
-                     ((1,self.realgrid,self.realgrid)))), axis=0)
+    return self.getGlobalView()
 
-    retStateExpanded = np.zeros((4, self.gridsize, self.gridsize), dtype=np.float32)
 
-    channel = 0
-    while channel < retState.shape[0]:
-        i = 0
-        while i < retState.shape[1]:
-            ii = 0
-            for ii in range(0,self.expansionRatio):
-                j = 0
-                while j < retState.shape[2]:
-                    jj = 0
-                    for jj in range(0,self.expansionRatio):
-                        retStateExpanded[channel][i*self.expansionRatio + ii][j*self.expansionRatio + jj] = retState[channel][i][j]
-                        jj += 1
-                    j += 1
-                ii += 1
-            i += 1
-        channel += 1
 
-    return retStateExpanded
 
 
   """    
+  """
+  def setFovealGoal(self,msg0,msg1):
+      # translate to global goal position for the loacl value
+      # set it as the goal
+      self.f_goal_x = self.ball_x0 + msg0 - 2
+      self.f_goal_y = self.ball_y0 + msg1 - 2
+      # return the environment view
+      return self.getLocalView()
+
+
+
+
+  """    
+  """
+  def getGlobalView(self):
+
+      retState = np.concatenate(
+          (np.reshape(self.state[0 * self.realgrid * self.realgrid: 1 * self.realgrid * self.realgrid],
+                      ((1, self.realgrid, self.realgrid))),
+           np.reshape(self.state[1 * self.realgrid * self.realgrid: 2 * self.realgrid * self.realgrid],
+                      ((1, self.realgrid, self.realgrid))),
+           np.reshape(self.state[2 * self.realgrid * self.realgrid: 3 * self.realgrid * self.realgrid],
+                      ((1, self.realgrid, self.realgrid))),
+           np.reshape(self.state[3 * self.realgrid * self.realgrid: 4 * self.realgrid * self.realgrid],
+                      ((1, self.realgrid, self.realgrid)))), axis=0)
+
+      retState = np.asarray(retState[:, self.ball_x0 - 2:self.ball_x0 + 3, self.ball_y0 - 2:self.ball_y0 + 3])
+
+      retStateExpanded = np.zeros((4, retState.shape[1] * self.expansionRatio, retState.shape[2] * self.expansionRatio),
+                                  dtype=np.float32)
+
+      channel = 0
+      while channel < retState.shape[0]:
+          i = 0
+          while i < retState.shape[1]:
+              ii = 0
+              for ii in range(0, self.expansionRatio):
+                  j = 0
+                  while j < retState.shape[2]:
+                      jj = 0
+                      for jj in range(0, self.expansionRatio):
+                          retStateExpanded[channel][i * self.expansionRatio + ii][j * self.expansionRatio + jj] = \
+                          retState[channel][i][j]
+                          jj += 1
+                      j += 1
+                  ii += 1
+              i += 1
+          channel += 1
+
+      return retStateExpanded
+
+
+
+
+  """    
+  """
+  def getLocalView(self):
+
+      retState = np.concatenate(
+          (np.reshape(self.state[0 * self.realgrid * self.realgrid: 1 * self.realgrid * self.realgrid],
+                      ((1, self.realgrid, self.realgrid))),
+           np.reshape(self.state[1 * self.realgrid * self.realgrid: 2 * self.realgrid * self.realgrid],
+                      ((1, self.realgrid, self.realgrid))),
+           np.reshape(self.state[2 * self.realgrid * self.realgrid: 3 * self.realgrid * self.realgrid],
+                      ((1, self.realgrid, self.realgrid))),
+           np.reshape(self.state[3 * self.realgrid * self.realgrid: 4 * self.realgrid * self.realgrid],
+                      ((1, self.realgrid, self.realgrid)))), axis=0)
+
+      retState = np.asarray(retState[:, self.f_goal_x - 2:self.f_goal_x + 3, self.f_goal_y - 2:self.f_goal_y + 3])
+
+      retStateExpanded = np.zeros((4, retState.shape[1] * self.expansionRatio, retState.shape[2] * self.expansionRatio),
+                                  dtype=np.float32)
+
+      channel = 0
+      while channel < retState.shape[0]:
+          i = 0
+          while i < retState.shape[1]:
+              ii = 0
+              for ii in range(0, self.expansionRatio):
+                  j = 0
+                  while j < retState.shape[2]:
+                      jj = 0
+                      for jj in range(0, self.expansionRatio):
+                          retStateExpanded[channel][i * self.expansionRatio + ii][j * self.expansionRatio + jj] = \
+                          retState[channel][i][j]
+                          jj += 1
+                      j += 1
+                  ii += 1
+              i += 1
+          channel += 1
+      return retStateExpanded
+
+
+
+  """
   """
   def step(self,msg):
 
@@ -178,6 +266,15 @@ class LmazeEnv(gym.Env):
         self.ball_y0 = self.ball_y0 + o_y
 
         self.state[0*self.realgrid*self.realgrid+(self.ball_x0)*self.realgrid+self.ball_y0] = 1.0
+
+        # if the goal goes out of view of agent after this movement
+        if (self.ball_x0 > self.f_goal_x - 2 or self.ball_x0 < self.f_goal_x +3 \
+                or self.ball_y0 > self.f_goal_y - 2 or self.ball_y0 < self.f_goal_y +3):
+            print(self.ball_x0 > self.f_goal_x - 2, self.ball_x0 < self.f_goal_x +3,
+                 self.ball_y0 > self.f_goal_y - 2, self.ball_y0 < self.f_goal_y +3)
+            self.stepCount = 0
+            self.localDone = True
+
         self.reward = self.positiveNominal
 
     elif self.grid[self.ball_x0 + o_x][self.ball_y0 + o_y] == 'X':
@@ -189,6 +286,10 @@ class LmazeEnv(gym.Env):
 
         self.state[0*self.realgrid*self.realgrid+(self.ball_x0)*self.realgrid+self.ball_y0] = 1.0
         self.reward = self.positiveFull
+
+
+
+
 
     if self.VISUALIZE:
         image = np.asarray(([200 if item == 1.0 else 0 for item in self.state[1 * self.realgrid * self.realgrid : 2 * self.realgrid * self.realgrid]]), dtype=np.uint8)
@@ -205,35 +306,22 @@ class LmazeEnv(gym.Env):
         plt.pause(.01)
         plt.draw()
 
-    retState = np.concatenate((np.reshape(self.state[0 * self.realgrid * self.realgrid : 1 * self.realgrid * self.realgrid],
-                     ((1,self.realgrid,self.realgrid))),
-                          np.reshape(self.state[1 * self.realgrid * self.realgrid : 2 * self.realgrid * self.realgrid],
-                     ((1,self.realgrid,self.realgrid))),
-                          np.reshape(self.state[2 * self.realgrid * self.realgrid : 3 * self.realgrid * self.realgrid],
-                     ((1,self.realgrid,self.realgrid))),
-                          np.reshape(self.state[3 * self.realgrid * self.realgrid : 4 * self.realgrid * self.realgrid],
-                     ((1,self.realgrid,self.realgrid)))), axis=0)
+    if self.stepCount == 10:
+        self.stepCount = 0
+        self.localDone = True
 
-    retStateExpanded = np.zeros((4, self.gridsize, self.gridsize), dtype=np.float32)
+    return self.getLocalView(), self.reward, self.isEpisodeFinished(), self.localDone, msg
 
-    channel = 0
-    while channel < retState.shape[0]:
-        i = 0
-        while i < retState.shape[1]:
-            ii = 0
-            for ii in range(0,self.expansionRatio):
-                j = 0
-                while j < retState.shape[2]:
-                    jj = 0
-                    for jj in range(0,self.expansionRatio):
-                        retStateExpanded[channel][i*self.expansionRatio + ii][j*self.expansionRatio + jj] = retState[channel][i][j]
-                        jj += 1
-                    j += 1
-                ii += 1
-            i += 1
-        channel += 1
 
-    return retStateExpanded, self.reward, self.isEpisodeFinished(), msg
+
+
+
+
+
+
+
+
+
 
     """    
     """
@@ -242,6 +330,6 @@ class LmazeEnv(gym.Env):
 
 
   def isEpisodeFinished(self):
-    if self.reward == self.positiveFull or self.stepCount == 100:
+    if self.reward == self.positiveFull:
         return True
     return False
